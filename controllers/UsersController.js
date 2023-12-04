25b8636c9f74e5daf8e39aa625643c4eabf8b848
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-12-03 16:02:58 -0800

    creating new user fnction API added

---
+++ controllers/UsersController.js
@@ -0,0 +1,44 @@
+import crypto from 'crypto';
+import dbClient from '../utils/db';
+
+const UsersController = {
+  async postNew(req, res) {
+    //const email = req.body ? req.body.email : string;
+    //const password = req.body ? req.body.password : string;
+    const email = req.body && req.body.email ? req.body.email : 'string';
+    const password = req.body && req.body.password ? req.body.password : 'string';
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    const userExists = await dbClient.db.collection('users').findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+
+    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+    try {
+      const result = await dbClient.db.collection('users').insertOne({
+        email,
+        password: hashedPassword,
+      });
+
+      const newUser = {
+        id: result.insertedId,
+        email,
+      };
+
+      return res.status(201).json(newUser);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  },
+};
+
+export default UsersController;
