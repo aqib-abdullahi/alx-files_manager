84074e272250b89217e211c6520ef64e18701e98
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-12-04 11:12:23 -0800

    server and workers added

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import routes from './routes';
+import controllerRouting from './routes/index';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+controllerRouting(app);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
+
+export default app;
